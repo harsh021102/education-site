fc319fa601edce1e5a6985257967db6885374285
harsh021102 <harsh021102@gmail.com>
2022-07-07 14:22:39 +0530

    change 3

--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -15,19 +15,19 @@ const SectionThree = () => {
             </div>
             <div data-aos="fade-down" data-aos-delay="500" className='text-center mx-auto grid grid-cols-2 w-11/12 md:w-5/12 mt-4 mb-12 gap-2 md:gap-2 '>
                 <div className='row-span-2 bg-white bg-opacity-20 backdrop-blur-lg rounded-xl drop-shadow-xl py-4 px-2'>
-                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2 animate-pulse'>6 M+</h1>
+                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2'>6 M+</h1>
                     <p className='font-light text-white text-2xl md:text-4xl text-center pb-4'>Happy students</p>
                 </div>
                 <div className='row-span-2 bg-white bg-opacity-20 backdrop-blur-lg rounded-xl drop-shadow-xl p-4'>
-                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2 animate-pulse'>13700 +</h1>
+                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2'>13700 +</h1>
                     <p className='font-light text-white text-2xl md:text-4xl text-center pb-4'>Video Lectures</p>
                 </div>
                 <div className='row-span-2 bg-white bg-opacity-20 backdrop-blur-lg rounded-xl drop-shadow-xl p-8'>
-                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2 animate-pulse'>2500 +</h1>
+                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2'>2500 +</h1>
                     <p className='font-light text-white text-2xl md:text-4xl text-center pb-4'>Mock Test</p>
                 </div>
                 <div className='row-span-2 bg-white bg-opacity-20 backdrop-blur-lg rounded-xl drop-shadow-xl p-8'>
-                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2 animate-pulse '>70000 +</h1>
+                    <h1 className='font-bold text-white text-4xl md:text-5xl text-center py-2'>70000 +</h1>
                     <p className='font-light text-white text-2xl md:text-4xl text-center pb-4'>Questions</p>
                 </div>
             </div>
